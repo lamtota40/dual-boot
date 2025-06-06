9540803591cf3677b2f60df04796b2c41a726b6a
lamtota40 <lamtota40@gmail.com>
2025-06-06 18:17:38 +0700

    Update grml.sh

--- grml.sh
+++ grml.sh
@@ -9,4 +9,5 @@ sudo mkdir -p /etc/grml/partconf
 sudo wget raw.githubusercontent.com/lamtota40/install-ulang/main/autorun-grml.sh -P /etc/grml/partconf
 sudo bash -c "echo 'CUSTOM_BOOTOPTIONS=\"ssh=pas123 dns=8.8.8.8,8.8.4.4 netscript=raw.githubusercontent.com/lamtota40/install-ulang/main/autorun-grml.sh toram\"' >> /etc/default/grml-rescueboot"
 sudo update-grub
+read -p "tekan [ENTER]"
 sudo grub-reboot 'Grml Rescue System (grml-small-2024.12-amd64.iso)'
