9461cdd438042e87f4916a0f4074a737a5cfb5c0
lamtota40 <lamtota40@gmail.com>
2025-06-10 16:54:53 +0700

    Update grml.sh

--- grml.sh
+++ grml.sh
@@ -9,7 +9,7 @@ sudo mkdir -p /boot/grml
  fi
 sudo mkdir -p /etc/grml/partconf
 sudo wget raw.githubusercontent.com/lamtota40/dual-boot/main/autorungrml.sh -P /etc/grml/partconf
-sudo bash -c "echo 'CUSTOM_BOOTOPTIONS=\"ssh=pas123 dns=8.8.8.8,8.8.4.4 netscript=raw.githubusercontent.com/lamtota40/install-ulang/main/autorun-grml.sh partconf=/dev/vda2 toram\"' >> /etc/default/grml-rescueboot"
+sudo bash -c "echo 'CUSTOM_BOOTOPTIONS=\"ssh=pas123 dns=8.8.8.8,8.8.4.4 netscript=raw.githubusercontent.com/lamtota40/install-ulang/main/autorun-grml.sh partconf=/dev/vda3 toram\"' >> /etc/default/grml-rescueboot"
 sudo update-grub
 read -p "tekan [ENTER] untuk reboot"
 sudo grub-reboot 'Grml Rescue System (grml-small-2024.12-amd64.iso)'
