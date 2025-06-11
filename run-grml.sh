41ebb6560b0f510de7d352e892ce8df4e7cbc0a9
lamtota40 <lamtota40@gmail.com>
2025-06-11 19:20:23 +0700

    Update and rename grml.sh to run-grml.sh

--- run-grml.sh
+++ run-grml.sh
@@ -7,9 +7,15 @@ sudo mkdir -p /boot/grml
  if [ ! -f /boot/grml/grml-small-2024.12-amd64.iso ]; then
   sudo wget https://mirror-hk.koddos.net/grml/grml-small-2024.12-amd64.iso -P /boot/grml/
  fi
-#sudo mkdir -p /etc/grml/partconf
-#sudo wget raw.githubusercontent.com/lamtota40/dual-boot/main/autorungrml.sh -P /etc/grml/partconf
-sudo bash -c "echo 'CUSTOM_BOOTOPTIONS=\"ssh=pas123 dns=8.8.8.8,8.8.4.4 netscript=raw.githubusercontent.com/lamtota40/install-ulang/main/autorun-grml.sh toram\"' >> /etc/default/grml-rescueboot"
+ 
+sudo tee /grml.sh > /dev/null << 'EOF'
+#!/bin/bash
+echo "Running..." >> /tmp/grml.lo
+touch /sukses.sh
+EOF
+sudo chmod +x /grml.sh
+
+sudo bash -c "echo 'CUSTOM_BOOTOPTIONS=\"ssh=pas123 dns=8.8.8.8,8.8.4.4 netscript=raw.githubusercontent.com/lamtota40/install-ulang/main/autorun-grml.sh scripts=/grml.sh toram\"' >> /etc/default/grml-rescueboot"
 sudo update-grub
 read -p "tekan [ENTER] untuk reboot"
 sudo grub-reboot 'Grml Rescue System (grml-small-2024.12-amd64.iso)'
